9ef32070989badc7cb0faa54322bd34995131b45
aceDavon <makojidavid@gmail.com>
2023-01-11 10:29:39 +0100

    Add user validations and relations
    Create the user entry validations and add user relationships to cars and
    reservations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,15 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable, :validatable
+
+  has_many :reservations, dependent: :destroy
+  has_many :cars
+
+  validates :name,
+            length: { minimum: 3, message: 'fields can not be less than 3 letters' }, presence: true
+  validates :email,
+            uniqueness: true, presence: true
+  validates :password,
+            length: { minimum: 6, message: 'can not be less than 6 characters' }, presence: true
+  validates :username,
+            length: { minimum: 6, message: 'can not be less than 6 characters' }, presence: true
 end
