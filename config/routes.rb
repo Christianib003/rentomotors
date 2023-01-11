e5c4c23f045e6c4998663bc26a92716aee0b6ba5
Habib Payenda <habiburahmanpayenda@gmail.com>
2023-01-12 03:19:50 +0430

    Adds resevation_controller and reservation routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :users, except: [:new]
+      resources :reservations, except: [:new]
     end
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
