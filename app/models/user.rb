763143193ed538aca2ab2cc20d7156162cc282f0
aceDavon <makojidavid@gmail.com>
2023-01-11 15:43:42 +0100

    Fix server errors with user update endpoint

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,8 +8,8 @@ class User < ApplicationRecord
             length: { minimum: 3, message: 'fields can not be less than 3 letters' }, presence: true
   validates :email,
             uniqueness: true, presence: true
-  validates :password,
-            length: { minimum: 6, message: 'can not be less than 6 characters' }
+  validates :encrypted_password,
+            length: { minimum: 6, message: 'can not be less than 6 characters' },presence: true
   validates :username,
             length: { minimum: 6, message: 'can not be less than 6 characters' }, presence: true
 end
