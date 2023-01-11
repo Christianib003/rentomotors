499941531bcdd5b4223ef0684c1bf59dbb17abd4
aceDavon <makojidavid@gmail.com>
2023-01-11 10:30:34 +0100

    Add database column constraints to user table
    Adding datavse level valudation to user table, refusing null values on
    username and name columns

--- db/migrate/20230110084629_create_users.rb
+++ db/migrate/20230110084629_create_users.rb
@@ -1,9 +1,9 @@
 class CreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
-      t.string :username, unique: true
+      t.string :username, unique: true, nullable: false
       t.string :encrypted_password, nullable: true
-      t.string :email, unique: true
+      t.string :email, unique: true, nullable: false
       t.string :name, default: 'user'
 
       t.timestamps
