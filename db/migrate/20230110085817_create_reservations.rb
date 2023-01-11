d99eed5d7137b54c89d066fbdeaa94c5fe7c0dc6
Habib Payenda <habiburahmanpayenda@gmail.com>
2023-01-12 02:56:39 +0430

    Adds columns to the reservation table

--- db/migrate/20230110085817_create_reservations.rb
+++ db/migrate/20230110085817_create_reservations.rb
@@ -2,7 +2,8 @@ class CreateReservations < ActiveRecord::Migration[7.0]
   def change
     create_table :reservations do |t|
       t.date :date
-
+      t.reserved_from :date
+      t.reserved_until :date
       t.timestamps
     end
   end
