425c0bb4e2992314cbfafb690b2e685a5b7d4aff
Habib Payenda <habiburahmanpayenda@gmail.com>
2023-01-13 19:25:49 +0430

    Adds the public cars routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
+      get 'cars', to: 'cars#all_cars'
       resources :users, except: [:new] do
         resources :cars
         resources :reservations
