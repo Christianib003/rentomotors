fa4808ec3cf38b727235a161bc5ee833e021a466
Habib Payenda <habiburahmanpayenda@gmail.com>
2023-01-13 19:30:58 +0430

    Fixes n+1 problem in reservation loading

--- app/controllers/api/v1/reservations_controller.rb
+++ app/controllers/api/v1/reservations_controller.rb
@@ -46,7 +46,7 @@ class Api::V1::ReservationsController < ApplicationController
   end
 
   def set_user
-    @user = User.find(params[:user_id])
+    @user = User.includes(:reservations).find(params[:user_id])
   end
 
   def set_reservation
