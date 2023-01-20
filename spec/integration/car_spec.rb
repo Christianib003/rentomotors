e64c4985580e1d0aec4e003e571f30cb5e74915b
Christian <christianib003@gmail.com>
2023-01-20 09:34:49 +0200

    add params to testing statement

--- spec/integration/car_spec.rb
+++ spec/integration/car_spec.rb
@@ -22,8 +22,8 @@ describe 'Car API' do
       }
       response '201', 'Car created' do
         let(:car) do
-          { brand: 'AlusiA', bike_image: "https://imgd.aeplcdn.com/1280x720/n/cw/ec/43482/sp-125-right-front-three-quarte
-r-2.jpeg?isig=0&q=80", bike_type: 'HNDA', description: 'Tption', brand: 'This Brand', daily_rate: '2000', user_id: 1 }
+          { brand: 'mercedes', image_link: "https://imgd.aeplcdn.com/1280x720/n/cw/ec/43482/sp-125-right-front-three-quarte
+r-2.jpeg?isig=0&q=80", model: 'benz', release_year: 1999, color: 'black', transmission: 'manual', seats: 6, wheel_drive: 'four-wheel', price: 20000, user_id: 1 }
         end
         run_test!
       end
@@ -41,19 +41,21 @@ r-2.jpeg?isig=0&q=80", bike_type: 'HNDA', description: 'Tption', brand: 'This Br
       response '200', 'name found' do
         schema type: :object,
                properties: {
-                 id: { type: :integer },
-                 name: { type: :string },
-                 bike_image: { type: :string },
-                 bike_type: { type: :string },
-                 description: { type: :string },
-                 brand: { type: :string },
-                 daily_rate: { type: :string },
-                 user_id: { type: :integer }
+                id: { type: :integer },
+                model: { type: :string },
+                release_year: { type: :integer },
+                color: { type: :string },
+                transmission: { type: :string },
+                price: { type: :integer },
+                seats: { type: :integer},
+                wheel_drive: { type: :string },
+                image_link: { type: :string },
+                user_id: { type: :integer }
                },
-               required: %w[id name bike_image bike_type description brand daily_rate user_id]
+               required: %w[id brand model release_year color transmission seats wheel_drive price user_id]
         let(:id) do
-          Bike.create(name: 'AlusiA', bike_image: "https://imgd.aeplcdn.com/1280x720/n/cw/ec/43482/sp-125-right-front-three-quarte
-r-2.jpeg?isig=0&q=80", bike_type: 'HNDA', description: 'Tption', brand: 'This Brand', daily_rate: '2000', user_id: 1).id
+          Car.create(brand: 'mercedes', image_link: "https://imgd.aeplcdn.com/1280x720/n/cw/ec/43482/sp-125-right-front-three-quarte
+          r-2.jpeg?isig=0&q=80", model: 'benz', release_year: 1999, color: 'black', transmission: 'manual', seats: 6, wheel_drive: 'four-wheel', price: 20000, user_id: 1).id
         end
         run_test!
       end
